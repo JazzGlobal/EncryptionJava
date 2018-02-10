97412625e964f588cfb7e9fbd8ad46c2d2402870
Christopher Gambrell <waymer147@gmail.com>
2018-02-09 23:51:57 -0500

    Update StringEncryption.java

--- StringEncryption.java
+++ StringEncryption.java
@@ -2,7 +2,6 @@ public class StringEncryption {
     public static void main(String[] args){
         Encryptor e = new Encryptor(20);
         System.out.println(e.getKey());
-        //System.out.println(e.EncryptedMessage("hey guys nick here im  a weak nigga that likes music and shit. go listen to the fire down"));
-        System.out.println(e.EncryptedMessage("chris is a gay retatattatata"));
+        System.out.println(e.EncryptedMessage("Hello, This is a test message!"));
     }
 }
